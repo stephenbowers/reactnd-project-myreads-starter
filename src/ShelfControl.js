072b86d1d33ab3e68fb9bfcce0c31132d0a2605d
Stephen Bowers <srb4887@gmail.com>
2018-09-25 18:45:52 -0700

    Change logic for starting shelf logic.

--- src/ShelfControl.js
+++ src/ShelfControl.js
@@ -4,7 +4,7 @@ class ShelfControl extends Component {
     render() {
         return(
             <div className="book-shelf-changer">
-                <select value={this.props.currentShelf} onChange={(event) => this.props.changeShelf(this.props.book, event.target.value)}>
+                <select defaultValue={this.props.book.shelf ? this.props.book.shelf : "none"} onChange={(event) => this.props.changeShelf(this.props.book, event.target.value)}>
                     <option value="move" disabled>Move to...</option>
                     <option value="currentlyReading">Currently Reading</option>
                     <option value="wantToRead">Want to Read</option>
